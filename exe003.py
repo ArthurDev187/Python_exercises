1c8c933669ca5ee230895918aa77adb0848ad6bb
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2024-10-05 13:29:31 +0800

    second commit

---
+++ exe003.py
@@ -0,0 +1,8 @@
+# 3.	Even or Odd
+# Write a program to check if a number is even or odd.
+number = int(input('Type a number to see if it is even or odd: '))
+if number % 2 == 0:
+    print(f'{number} is Even.')
+else:
+    print(f'{number} is Odd.')
+    
