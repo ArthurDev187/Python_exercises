868c4cc34fdc30bc9ab3eff6cbd84acb9fac6005
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-02-23 15:08:54 +0800

    data types

---
+++ w3resourcesExercises/data_types.py
@@ -0,0 +1,44 @@
+dicionario = dict()
+dicionario['nome', 'idade'] =  'Arthur', 30
+print(type(dicionario))
+print(dicionario)
+
+tipo_set = {8, 'abc', 'Arthur', 8.3, True}
+print(type(tipo_set))
+print(tipo_set)
+
+lista = list()
+lista = ['5', 5, 'Lua', 'Mar', 7.1]
+print(type(lista))
+print(lista)
+
+inteiro = 9
+print(type(inteiro))
+print(inteiro)
+
+flutuante = 9.9
+print(type(flutuante))
+print(flutuante)
+
+tupla = ('Aqui temos uma tupla', 748, 84.2, 'Bom trabalho')
+print(type(tupla))
+print(tupla)
+
+texto = 'Aqui é uma vareável de texto.'
+print(type(texto))
+print(texto)
+
+set_fixo = frozenset()
+set_fixo = 98, 'set_fixo', 765.2
+print(type(set_fixo))
+print(set_fixo)
+
+boolean = bool()
+boolean = True
+print(type(boolean))
+print(boolean)
+
+bytess = bytes()
+print(type(bytess))
+bytess = 10010101110101
+print(bytess)
