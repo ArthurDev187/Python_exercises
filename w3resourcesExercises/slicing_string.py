ac9ba0899b8c54e5942a8c292fbf05adec7ba08e
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-02-26 21:20:02 +0800

    Scape character

--- w3resourcesExercises/slicing_string.py
+++ w3resourcesExercises/slicing_string.py
@@ -1,8 +1,11 @@
 a = 'Hello World!'
 
-#print(a[-5: -2: 3])
+# print(a[-5: -2: 3])
 
 b = 'O rato roeu a roupa do rei de roma na rampa rodada de rema na romênia'
 
 c = b[-42 : -5 : 2]
-print(c)
+# print(c)
+
+txt = "We are so called \"Vikings\" in the north."
+print(txt)
