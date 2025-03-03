bd61613cc73446e98b954d45f2f1043ba4f00502
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-03-03 12:48:31 +0800

    comma for thousand separation

--- w3resourcesExercises/format_string.py
+++ w3resourcesExercises/format_string.py
@@ -1,8 +1,13 @@
 # valor = 70
 
 # print(f'It is very {'expensive' if valor > 50 else 'cheap'}!')
+##############################
 
-def convert_celcius(faren):
-    return (faren - 32) * 5/9
+# def convert_celcius(faren):
+#     return (faren - 32) * 5/9
 
-print(f'Valor de F° convertido em C°: {convert_celcius(75):.1f}')
+# print(f'Valor de F° convertido em C°: {convert_celcius(75):.1f}')
+
+price = 2505600
+
+print(f'The price is: ${price:,}')
