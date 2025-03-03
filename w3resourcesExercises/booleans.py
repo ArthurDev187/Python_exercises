ab30c4aec7d75aef754e7902765db0e6a8880b36
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-03-03 18:53:13 +0800

    Print a message based on whether the condition is True or False

--- w3resourcesExercises/booleans.py
+++ w3resourcesExercises/booleans.py
@@ -1,7 +1,19 @@
 #testing some conditions with booleans return
-print(10 > 9)
-print(5 == 5)
-print(5 == '5')
-print('st' == 'st')
-print(5.0 == 5)
-print(None == True)
+# print(10 > 9)
+# print(5 == 5)
+# print(5 == '5')
+# print('st' == 'st')
+# print(5.0 == 5)
+# print(None == True)
+
+# print('\n\n')
+
+# Print a message based on whether the condition is True or False:
+
+a = 200
+b = 300
+
+if a > b:
+    print('The value of a is bigger than b')
+else:
+    print('The value of b is bigger than a')
