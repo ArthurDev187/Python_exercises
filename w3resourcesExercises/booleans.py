a1498cda790eb40caaf326e459341b94e5d8e834
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-03-03 19:01:53 +0800

    Some Values are False

--- w3resourcesExercises/booleans.py
+++ w3resourcesExercises/booleans.py
@@ -21,5 +21,18 @@
 
 # Evaluate Values and Variables
 
-print(bool('Hello'))
-print(bool(None))
+# print(bool('Hello'))
+# print(bool(None))
+
+
+
+# One more value, or object in this case, evaluates to False, and that is if you have 
+# an object that is made from a class with a __len__ function that returns 0 or False:
+
+class myclass():
+    def __len__(self):
+        return 0
+    
+
+myobj = myclass()
+print(bool(myobj))
