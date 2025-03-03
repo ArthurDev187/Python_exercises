cbd9a81b58731d19fb06507a3e8c62e50a5b7864
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-03-03 12:45:45 +0800

    using a func inside a print string

--- w3resourcesExercises/format_string.py
+++ w3resourcesExercises/format_string.py
@@ -1,3 +1,8 @@
-valor = 70
+# valor = 70
 
-print(f'It is very {'expensive' if valor > 50 else 'cheap'}!')
+# print(f'It is very {'expensive' if valor > 50 else 'cheap'}!')
+
+def convert_celcius(faren):
+    return (faren - 32) * 5/9
+
+print(f'Valor de F° convertido em C°: {convert_celcius(75):.1f}')
