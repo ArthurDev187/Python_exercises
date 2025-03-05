4c72a23c723289634a2ca4b3c23fea150407cc40
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-03-05 22:09:14 +0800

    Selecting items in the list

--- w3resourcesExercises/lists.py
+++ w3resourcesExercises/lists.py
@@ -1,8 +1,17 @@
 this_list = ['Banana', 'Apple', 'Cherry', 'Apple', 'Cherry', 7384, 43.4, True]
-print(len(this_list))
-print(type(this_list))
+# print(len(this_list))
+# print(type(this_list))
 
-print()
-another_list = list(('list', 'blackberry', 1234, False))
-print(another_list)
-print(type(another_list))
+# print()
+# another_list = list(('list', 'blackberry', 1234, False))
+# print(another_list)
+# print(type(another_list))
+
+
+print(this_list[4])
+print(this_list[-1])
+print(this_list[-7])
+print(this_list[3:7])
+print(this_list[2:-2])
+print(this_list[::2])
+print(this_list[::-2])
