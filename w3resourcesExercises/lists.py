fe2e76902ac97d2051ebd915829e467a2cda1b07
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-03-05 22:04:41 +0800

    Introduction of lists

---
+++ w3resourcesExercises/lists.py
@@ -0,0 +1,8 @@
+this_list = ['Banana', 'Apple', 'Cherry', 'Apple', 'Cherry', 7384, 43.4, True]
+print(len(this_list))
+print(type(this_list))
+
+print()
+another_list = list(('list', 'blackberry', 1234, False))
+print(another_list)
+print(type(another_list))
