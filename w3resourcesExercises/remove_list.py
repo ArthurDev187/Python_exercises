2ba4116162018b67b6c7fd334d4b92d71950942a
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-03-09 20:14:31 +0800

    I learned how to remove items from a lists and everthing else

---
+++ w3resourcesExercises/remove_list.py
@@ -0,0 +1,30 @@
+thislist = ["apple", "banana", "cherry", "banana", "orange"]
+
+# The remove() method removes the specified item
+
+# thislist.remove('banana')
+# print(thislist)
+
+# Remove the first occurrence of "banana":
+# thislist.remove('banana')
+# print(thislist)
+
+# The pop() method removes the specified index.
+
+# thislist.pop(2)
+# print(thislist)
+
+# The del keyword also removes the specified index:
+
+# del thislist[0]
+# print(thislist)
+
+# The del keyword can also delete the list completely.
+
+# del thislist
+# print(thislist)
+
+# The clear() method empties the list. The list still remains, but it has no content.
+
+thislist.clear()
+print(thislist)
