9fd7392d12b61f8f872bccd243a8124c6a7f362c
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-03-24 21:04:52 +0800

    removing items from a dictionary

---
+++ w3resourcesExercises/dictionaries/dict_remove_items.py
@@ -0,0 +1,39 @@
+"""Removing Items
+There are several methods to remove items from a dictionary:
+
+Example
+The pop() method removes the item with the specified key name:"""
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# thisdict.pop('model')
+# print(thisdict)
+
+"""Example
+The popitem() method removes the last inserted item (in versions before 3.7, 
+a random item is removed instead):"""
+
+# thisdict.popitem()
+# print(thisdict)
+
+"""Example
+The del keyword removes the item with the specified key name:"""
+
+# del thisdict['model']
+# print(thisdict)
+
+"""Example
+The del keyword can also delete the dictionary completely:"""
+
+# del thisdict
+# print(thisdict)
+
+"""Example
+The clear() method empties the dictionary:"""
+
+thisdict.clear()
+print(thisdict)
